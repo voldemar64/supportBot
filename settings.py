229e7ff0cd4a8205f79d52085281ff089a3dc584
Vladimir Kostyuk <voldemar22864@gmail.com>
2023-08-11 22:51:38 +0300

    fixed: getting env variables

--- settings.py
+++ settings.py
@@ -4,18 +4,18 @@ from dotenv import load_dotenv, find_dotenv
 # Loading .env variables
 load_dotenv(find_dotenv())
 
-TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 if TELEGRAM_TOKEN is None:
     raise Exception("Please setup the .env variable TELEGRAM_TOKEN.")
 
 PORT = int(os.environ.get('PORT', '8443'))
 
-TELEGRAM_SUPPORT_CHAT_ID = os.environ["TELEGRAM_SUPPORT_CHAT_ID"]
+TELEGRAM_SUPPORT_CHAT_ID = os.environ.get("TELEGRAM_SUPPORT_CHAT_ID")
 if TELEGRAM_SUPPORT_CHAT_ID is None or not str(TELEGRAM_SUPPORT_CHAT_ID).lstrip("-").isdigit():
     raise Exception("You need to specify 'TELEGRAM_SUPPORT_CHAT_ID' env variable: The bot will forward all messages to this chat_id. Add this bot https://t.me/ShowJsonBot to your private chat to find its chat_id.")
 TELEGRAM_SUPPORT_CHAT_ID = int(TELEGRAM_SUPPORT_CHAT_ID)
 
 
-WELCOME_MESSAGE = os.environ["WELCOME_MESSAGE"]
-REPLY_TO_THIS_MESSAGE = os.environ["REPLY_TO_THIS_MESSAGE"]
-WRONG_REPLY = os.environ["WRONG_REPLY"]
+WELCOME_MESSAGE = os.environ.get("WELCOME_MESSAGE")
+REPLY_TO_THIS_MESSAGE = os.environ.get("REPLY_TO_THIS_MESSAGE")
+WRONG_REPLY = os.environ.get("WRONG_REPLY")
